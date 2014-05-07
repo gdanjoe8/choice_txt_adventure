b719e76e11b45d649f158286e2f565a7c09b9e1f
gdanjoe8 <gdanjoe8@vt.edu>
2014-05-07 10:25:48 -0400

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,23 @@
+#include <sctring>
+
+using namespace std::
+
+class Player
+{
+private:
+	string play_name;
+	string player_name;
+	int player_power;
+	int player_accuracy;
+	int player_agility;
+public:
+	Player(string);
+	void setP_Name(string);
+	void setP_Power(int);
+	void setP_Agility(int);
+	void setP_Accuracy(int);
+	int getP_Power();
+	int getP_Agility();
+	int getP_Accuracy();
+	int getP_Name();
+}
