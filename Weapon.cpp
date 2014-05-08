4d39bd72f6bbe5a6945b61b94349cdf937589ee8
(540) 232-6724 <gdanjoe8@ece2524.ece.vt.edu>
2014-05-08 12:03:24 -0400

    beginning of project

---
+++ Weapon.cpp
@@ -0,0 +1,39 @@
+#include "Weapon.h"
+
+Weapon::Weapon(string name, int power, int agil, int accur)
+{
+	weapon_name = name;
+	weapon_power = power;
+	weapon_agility = agil;
+	weapon_accuracy = accur;
+}
+
+void Weapon::setPower(int power)
+{
+	weapon_power = power;
+}
+
+int Weapon::getPower()
+{
+	return weapon_power;
+}
+
+void Weapon::setAgility(int agil)
+{
+	weapon_agility = agil;
+}
+
+int Weapon::getAgility()
+{
+	return weapon_agility;
+}
+
+void Weapon::setAccuracy(int accur)
+{
+	weapon_accuracy = accur;
+}
+
+int Weapon::getAccuracy()
+{
+	return weapon_accuracy;
+}
---
+++ Weapon.h
@@ -0,0 +1,20 @@
+#include <string>
+
+using namespace std::
+
+class Weapon {
+private:
+	string weapon_name;
+	int weapon_power;
+	int weapon_agility;
+	int weapon_accuracy;
+public:
+	Weapon(string, int, int ,int);
+	void setPower(int);
+	int getPower();
+	void setAgility(int);eap
+	int getAgility(int);
+	void setAccuracy(int);
+	int getAccuracy(int);
+
+}
